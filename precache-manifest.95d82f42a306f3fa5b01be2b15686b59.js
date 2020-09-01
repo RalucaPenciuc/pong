bdc19aa294cfd7bed66fc2586bc5f59d8936cd82
Raluca <penciuc.raluca@yahoo.com>
2020-09-01 12:55:27 +0300

    Updates

---
+++ precache-manifest.95d82f42a306f3fa5b01be2b15686b59.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "398805e5fa46a4a1a32b4532386a6e65",
+    "url": "/pong-game/index.html"
+  },
+  {
+    "revision": "507480d9c0f1e82ebac6",
+    "url": "/pong-game/static/css/main.5319b03d.chunk.css"
+  },
+  {
+    "revision": "980eaca337ccf1ed84c2",
+    "url": "/pong-game/static/js/2.16bc9eaa.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/pong-game/static/js/2.16bc9eaa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "507480d9c0f1e82ebac6",
+    "url": "/pong-game/static/js/main.c2e53548.chunk.js"
+  },
+  {
+    "revision": "1ee402f15f1a9e4b0df2",
+    "url": "/pong-game/static/js/runtime-main.8f3be364.js"
+  }
+]);
